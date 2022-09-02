5c154fd9458192e9bcd9804f4779e840591a65b1
ikrambej <ikram.bejaoui@esprit.tn>
2022-09-02 16:40:55 +0200

    Create public subnet 1

--- terraform.tfvars
+++ terraform.tfvars
@@ -1,2 +1,3 @@
-region   = "eu-west-3"
-vpc_cidr = "172.16.0.0/16"
+region              = "eu-west-3"
+vpc_cidr            = "172.16.0.0/16"
+pub_sub1_cidr_block = "172.16.1.0/24"
--- variables.tf
+++ variables.tf
@@ -9,3 +9,8 @@ variable "vpc_cidr" {
   type        = string
   #default     = "172.16.0.0/16"
 }
+
+variable "pub_sub1_cidr_block" {
+  description = "Public subnet 1"
+  type        = string
+}
